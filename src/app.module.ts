6a2e6267f06bc0d41d1ecb09003023e5a22af7b9
Carlosvpm <moraesvinicius_2002@hotmail.com>
2021-12-28 10:52:04 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://db-user:12345@cluster0.tvsyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
